7842dd1d835b7c7cc43f2865bab51feea01a57e2
tloyan <thomas.loyan@gmail.com>
2024-12-16 20:05:15 +0100

    feat(codewars): wait for selector user_email

--- src/CodeWars.ts
+++ src/CodeWars.ts
@@ -18,6 +18,7 @@ export class CodeWars {
   private async login() {
     const page = await this.browser?.newPage()!
     await page.goto("https://www.codewars.com/users/sign_in")
+    await page?.waitForSelector("#user_email", { timeout: 60000 })
     await page.type("#user_email", process.env.CODEWARS_USER_EMAIL)
     await page.type("#user_password", process.env.CODEWARS_USER_PASSWORD)
     await page.click("button[type='submit']")
